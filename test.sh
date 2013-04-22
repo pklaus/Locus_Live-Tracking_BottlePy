c4a2cb29c58ec10050622ff302df062ecb4b48fb
Philipp Klaus <philipp.klaus@gmail.com>
2013-04-22 15:16:52 +0200

    A rough tool to feed the server with test data

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LAT=$((RANDOM/3000)).$RANDOM
+LON=$((RANDOM/3000)).$RANDOM
+ALT=$((RANDOM/30))
+SPEED=$((RANDOM/300+30))
+ACC=$((RANDOM/3000+5))
+BEARING=$((RANDOM/100))
+TIME=$(date +%s)000
+
+wget \
+  -O - \
+  --post-data \
+  "lat=$LAT&lon=$LON&alt=$ALT&speed=$SPEED&acc=$ACC&bearing=$BEARING&time=$TIME" \
+  http://localhost:8080/event
