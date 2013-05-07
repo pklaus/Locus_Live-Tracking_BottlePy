f95bf5c7a4b72d6a757e030c1e20d330b04bbd0b
Philipp Klaus <philipp.klaus@gmail.com>
2013-05-07 14:25:52 +0200

    Implementing the CLI
    $ ./app.py -h
    usage: app.py [-h] [-p PORT] [-6] [-d] [-b DB_FILE]
    
    Start a server to store location information.
    
    optional arguments:
      -h, --help            show this help message and exit
      -p PORT, --port PORT  The port to run the web server on.
      -6, --ipv6            Listen to incoming connections via IPv6 instead of
                            IPv4.
      -d, --debug           Start in debug mode (with verbose HTTP error pages.
      -b DB_FILE, --db-file DB_FILE
                            The db to store the location information in.

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ from datetime import datetime
 from random import choice
 import string
 import re
+import argparse
 
 TEMPLATE_PATH.append(os.path.join(os.path.split(os.path.realpath(__file__))[0],'views'))
+DEFAULT_DB_FILE = os.path.join(os.path.split(os.path.realpath(__file__))[0],"data/events.dict.sqlite")
 
-events = filedict.FileDict(filename="data/events.dict.sqlite")
+events = dict()
 
 filter_dict = {}
 view = partial(jinja2_view,
@@ -82,7 +84,27 @@ def static(path):
 def index():
     return dict(events=[events[key] for key in events])
 
-run(interface, host='0.0.0.0', port=8080, debug=True, reloader=True)
-# CherryPy is Python3 ready and has IPv6 support:
-#run(interface, host='::', server='cherrypy', port=8080)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( 
+      description='Start a server to store location information.' )
+    parser.add_argument('-p', '--port', type=int, default=8080,
+      help='The port to run the web server on.')
+    parser.add_argument('-6', '--ipv6', action='store_true',
+      help='Listen to incoming connections via IPv6 instead of IPv4.')
+    parser.add_argument('-d', '--debug', action='store_true',
+      help='Start in debug mode (with verbose HTTP error pages.')
+    parser.add_argument('-b', '--db-file', default=DEFAULT_DB_FILE,
+      help='The db to store the location information in.')
+    args = parser.parse_args()
+    if args.debug and args.ipv6:
+        args.error('You cannot use IPv6 in debug mode, sorry.')
+    events = filedict.FileDict(filename=args.db_file)
+    if args.debug:
+        run(interface, host='0.0.0.0', port=args.port, debug=True, reloader=True)
+    else:
+        if args.ipv6:
+            # CherryPy is Python3 ready and has IPv6 support:
+            run(interface, host='::', server='cherrypy', port=args.port)
+        else:
+            run(interface, host='0.0.0.0', server='cherrypy', port=args.port)
 
